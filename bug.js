0dea265fe629cbb463091da492906da23a7d25b6
BugHunter-X <askzenai@gmail.com>
2025-01-07 04:22:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,39 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <Link href='/about'>
+        <a>Go to About Page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+import fetch from 'node-fetch'; // This is the problematic line
+
+export async function getServerSideProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+export default function About({ data }) {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Todo Title: {data.title}</p>
+    </div>
+  );
+}
+```
